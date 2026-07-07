758dc283632aa33abdcf335ecbe0dda6e10995ea
liem0352 <2644046556@qq.com>
2026-07-08 07:30:17 +0800

    上传 vue-router-practice/src/App.vue

---
+++ vue-router-practice/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>根组件主页</h1>
+    <router-link to="/Home">Home 首页</router-link>
+    <router-link to="/About">关于我们</router-link>
+    <router-view></router-view>
+    <a href=""></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+/* 你可以在这里添加一些样式 */
+</style>
+
