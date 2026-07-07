388b0a952ee6e328284cc303476341b2e2b50879
liem0352 <2644046556@qq.com>
2026-07-08 07:30:59 +0800

    上传 slot-practice/练习1/Dslot.vue

---
+++ slot-practice/练习1/Dslot.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h5>子组件中的插槽传值给父组件</h5>
+        <ol>
+           <li v-for="(item, index) in items" :key="index">
+             <slot :itemName="item" name="tom"></slot>
+           </li>
+        </ol>
+    </div>
+</template>
+<script>
+import { reactive, toRefs } from 'vue';
+
+export default {
+    setup() {
+        const data = reactive({
+            items: ['Feed a cat', 'Buy milk', '吃麻辣烫']
+        });
+        return { ...toRefs(data) };
+    }
+}
+</script>
