20b5db2de50f321929fb419889316a331442ac20
liem0352 <2644046556@qq.com>
2026-07-08 07:31:07 +0800

    上传 slot-practice/练习2/Cslot2.vue

---
+++ slot-practice/练习2/Cslot2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="s2">
+        <div>插槽应用-子组件2</div>
+        <hr>
+        <slot name="first"></slot>
+        <hr>
+        <slot name="third"></slot>
+        <hr>
+        <slot name="second">我是老二</slot>
+        <div>上面是插槽位置</div>
+    </div>
+</template>
+
+<style>
+.s2 {
+    flex: 1;
+    border: 2px dashed rgb(3, 229, 55);
+}
+</style>
