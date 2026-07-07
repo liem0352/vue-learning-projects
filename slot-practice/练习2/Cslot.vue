c934609515ff3cb204ff6a6f0a9b41bca6a77094
liem0352 <2644046556@qq.com>
2026-07-08 07:31:05 +0800

    上传 slot-practice/练习2/Cslot.vue

---
+++ slot-practice/练习2/Cslot.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="s1">
+        <div>插槽应用-子组件1</div>
+        <hr>
+        <slot></slot>
+        <hr>
+        <slot name="third"></slot>
+        <hr>
+        <slot name="second">我是老二</slot>
+        <div>上面是插槽位置</div>
+    </div>
+</template>
+
+<style>
+.s1 {
+    flex: 1;
+    border: 2px dashed hotpink;
+}
+</style>
