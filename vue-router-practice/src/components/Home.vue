88f4be839e33a5e61f44a034ef17f38e043d52b8
liem0352 <2644046556@qq.com>
2026-07-08 07:30:28 +0800

    上传 vue-router-practice/src/components/Home.vue

---
+++ vue-router-practice/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-container">
+    <h2>Home 组件</h2>
+    <p>欢迎来到我们的首页！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home-container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
