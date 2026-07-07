1449914ebd6b3867b8e8bf93096660f349f6be4a
liem0352 <2644046556@qq.com>
2026-07-08 07:30:57 +0800

    上传 slot-practice/练习1/Dparent.vue

---
+++ slot-practice/练习1/Dparent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h5>父插槽</h5>
+        <todo-list>
+            <template v-slot:tom="slotProps">
+                 <p>子组件接收的数据在这里:{{ slotProps.itemName }}</p>
+            </template>
+        </todo-list>
+    </div>
+</template>
+
+<script>
+import Dslot from './Dslot.vue';
+
+export default {
+    components: { 'todo-list': Dslot }
+}
+</script>
