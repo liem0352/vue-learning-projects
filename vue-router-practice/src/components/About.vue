406fc3cde54234c43cc01d8fcb1c07dc4d601f6c
liem0352 <2644046556@qq.com>
2026-07-08 07:30:25 +0800

    上传 vue-router-practice/src/components/About.vue

---
+++ vue-router-practice/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-container">
+    <h2>About 组件</h2>
+    <p>这是关于我们页面的内容。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.about-container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
